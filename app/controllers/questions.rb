5c43b3a6177919f00deb484142aad0b41e920a93
Kevin Huang <kevinziwenhuang@gmail.com>
2016-02-18 14:55:36 -0800

    Added files via upload

---
+++ app/controllers/questions.rb
@@ -0,0 +1,40 @@
+get '/questions' do
+  erb :'/questions'
+end
+
+post '/questions' do
+  question = Post.create(title: params[:title], user: current_user)
+  if request.xhr?
+    
+  else
+    redirect "/questions/#{question.id}"
+  end
+end
+
+get '/questions/:id' do
+  @question = Post.find_by(id: params[:id])
+  erb :'/questions/show'
+end
+
+get '/questions/:id/edit' do
+  if logged_in?
+  @question = Post.find_by(id: params[:id])
+  if request.xhr?
+    erb :'_form', :layout => false
+  else
+  erb :'_form'
+  end
+  else
+    redirect "/questions/#{params[:id]}"
+  end
+end
+
+put '/questions/:id' do
+  @question = Post.find_by(id: params[:id])
+  if request.xhr?
+    erb :'_question', :layout => false 
+  else
+    @question.update(title: params[:title])
+    redirect "/questions/#{question.id}"
+  end
+end
