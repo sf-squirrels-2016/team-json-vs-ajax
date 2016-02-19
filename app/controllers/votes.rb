b6b36f2d74c076d3e80ebed3404eb87adf6c8d1a
Gary Wong <email": null>
2016-02-19 13:45:14 -0800

    Add sum of votes for answers

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -17,3 +17,23 @@ get '/questions/:id/votes/minus' do
   redirect "/questions/#{params[:id]}"
 end
 
+get '/answers/:id/votes/add' do
+  answer_id = Answer.find(params[:id])
+  p '/////////////////////////params answer///////////////////'
+  p params[:id]
+  p answer_id
+  user = User.find(session[:user_id])
+  p '/////////////////////////////////// session id'
+  p session[:user_id]
+  my_vote = Vote.new(user: user, votable: answer_id, vote_value:1)
+  my_vote.save
+  redirect "/"
+end
+
+# get '/answers/:id/votes/minus' do
+#   answer_id = Answer.find(params[:id])
+#   user = User.find(session[:user_id])
+#   my_vote = Vote.new(user: user, votable: answer_id, vote_value:-1)
+#   my_vote.save
+#   redirect "/questions/#{params[:id]}"
+# end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,5 @@ class Answer < ActiveRecord::Base
     belongs_to :user
     belongs_to :question
     has_many   :comments, :as => :commentable
+    has_many   :votes, :as => :votable
 end
