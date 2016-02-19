0279ae200ca2c7311796ba19f18c58afac4aa451
Timur Catakli <timurcatakli@gmail.com>
2016-02-19 12:32:48 -0800

    Show comment form

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
 
+  $('.comment_div').hide();
+
+    // Capture Comment Form Submission
+    $('.comment_show_button').on("click", function(e) {  
+        e.preventDefault();
+        console.log($(this).next().next().next());
+        $(this).next().next().next().show();
+        
+    });
+    // Capture Comment Form Submission
+
+
 });
