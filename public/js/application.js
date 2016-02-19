3c2b22be66b89b6020de6836d8d7ef99f9a34a5e
Timur Catakli <timurcatakli@gmail.com>
2016-02-19 15:33:35 -0800

    Fix comment ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,50 @@
 $(document).ready(function () {
 
-  $('.comment_div').hide();
+    $('.main_comment_div').hide();
 
-    // Capture Comment Form Submission
+    // Capture Show Form Button
     $('.comment_show_button').on("click", function(e) {  
         e.preventDefault();
         console.log($(this).next().next().next());
-        $(this).next().next().next().show();
-        
+        $(this).next().next().next().show();        
     });
-    // Capture Comment Form Submission
+    // Capture Show Form Button
+
+    // Capture Comment Form
+    $('.comment_form').on("submit", function(e) {  
+        e.preventDefault();
+        var $uri = $(this).attr('action');
+        var $data = $(this).serialize();
+
+          console.log("=======================")
+          console.log($uri)
+          console.log($data)
+          console.log("=======================")
+
+        // ajax request starts
+        var ajaxRequest = $.ajax({
+          url: $uri,
+          type: 'POST',
+          data: $data
+        });
+        // ajax request ends
+
+      //ajax success response
+      ajaxRequest.done(function(serverResponse, status, jqXHR) {
+        serverResponse.comment
+        var div ="<div class='row' style='text-align: left; margin-top: 20px;'>" + 
+        "<span class='glyphicon glyphicon-user' aria-hidden='true'></span> " + serverResponse.comment + "</div>"
+
+        $('.comments_div').append(div);
+        $('.main_comment_div').hide();
+        $('.textArea').val('');
+
+
+
+      });
+
+    });
+    // Capture Comment Form
 
 
 });
