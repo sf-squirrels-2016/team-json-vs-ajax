0e1f0eeb78c392142f9c9605085c5aa8482e8c5a
Kevin Huang <kevin.ziwen.huang@gmail.com>
2016-02-19 12:35:41 -0800

    Add answer form

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,13 +1,18 @@
-post '/questions/:question_id/answers' do 
-	answer = Answer.create(body: params[:body], user: current_user, question: Question.find(params:question_id))
+post '/questions/:id/answers' do 
+	answer = Answer.create(body: params[:body], user_id: session[:user_id], question: Question.find(params[:id]))
 	if answer.save 
 		if request.xhr?
 			erb :"_answer_row", :layout=> false, locals: { answer: answer }
 		else
-			redirect "/questions/#{params[question_id]}"
+			redirect "/questions/#{params[:id]}"
 		end
 	else
 		# show errors here. 
 	end
 
+end
+
+get '/questions/:id/answers/new' do
+  @question = Question.find(params[:id])
+ erb :'/answers/_form', :layout => false, locals:{question_id:@question.id}
 end
